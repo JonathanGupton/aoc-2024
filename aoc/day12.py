f76cff85e9ac090e61c8f5c1de89a4611e24b7f5
Jonathan Gupton <jonathan.gupton@gmail.com>
2024-12-12 19:31:35 -0600

    feat: add day 12 solutions

--- aoc/day12.py
+++ aoc/day12.py
@@ -1,8 +1,17 @@
 from collections import deque
 from itertools import product
+from typing import Generator
 from typing import Sequence
 
 DIRECTIONS = (complex(1, 0), complex(0, 1), complex(-1, 0), complex(0, -1))
+HORIZONTAL_DIRECTIONS = [complex(1, 0), complex(-1, 0)]
+VERTICAL_DIRECTIONS = [complex(0, -1), complex(0, 1)]
+DIRECTIONS_PERPENDICULAR = {
+    HORIZONTAL_DIRECTIONS[0]: VERTICAL_DIRECTIONS,
+    HORIZONTAL_DIRECTIONS[1]: VERTICAL_DIRECTIONS,
+    VERTICAL_DIRECTIONS[0]: HORIZONTAL_DIRECTIONS,
+    VERTICAL_DIRECTIONS[1]: HORIZONTAL_DIRECTIONS,
+}
 
 
 def parse_data(fp: str) -> tuple[dict[complex, str], tuple[int, int]]:
@@ -17,7 +26,7 @@ def parse_data(fp: str) -> tuple[dict[complex, str], tuple[int, int]]:
 
 def next_directions(
     start: complex, directions: tuple[complex, ...] = DIRECTIONS
-) -> complex:
+) -> Generator[complex, None, None]:
     for direction in directions:
         yield start + direction
 
@@ -57,26 +66,140 @@ def get_plot_areas(
     return plot_collections
 
 
-def compute_single_plot_perimeter(
-    single_plot: complex, garden_plots: dict[complex, str]
-) -> int:
-    pass
+def compute_plot_perimeter(plot: set[complex]) -> int:
+    perimeter = 0
+    for single_plot in plot:
+        for direction in next_directions(single_plot):
+            if direction not in plot:
+                perimeter += 1
+    return perimeter
 
 
-def compute_plot_perimeter(plot: set[complex], garden_plots: dict[complex, str]) -> int:
-    pass
-
-
-def get_full_plot_perimeters(
-    plot_areas: Sequence[set[complex]], garden_plots: dict[complex, str]
-) -> list[int]:
+def get_full_plot_perimeters(plot_areas: Sequence[set[complex]]) -> list[int]:
     perimeters = []
     for area in plot_areas:
-        perimeters.append(compute_plot_perimeter(area, garden_plots))
+        perimeters.append(compute_plot_perimeter(area))
     return perimeters
 
 
-if __name__ == "__main__":
+def traverse_fence_edge(
+    start: complex, area: set[complex], perpendicular_direction: complex
+) -> frozenset[tuple[complex, complex]]:
+    seen = set()
+    for parallel in DIRECTIONS_PERPENDICULAR[perpendicular_direction]:
+        q = deque()
+        q.append(start)
+        while q:
+            current = q.popleft()
+            seen.add((current, current + perpendicular_direction))
+            if (
+                current + parallel in area
+                and (current + parallel + perpendicular_direction) not in area
+            ):
+                current = current + parallel
+                q.append(current)
+    return frozenset(seen)
+
+
+def count_continuous_plot_edges(area: set[complex]) -> int:
+    """Part B to find continuous perimeters"""
+    edges: set[frozenset[tuple[complex, complex]]] = set()
+    for plot in area:
+        for direction in DIRECTIONS:
+            if plot + direction not in area:  # it's an edge
+                edges.add(traverse_fence_edge(plot, area, direction))
+    return len(edges)
+
+
+def get_continuous_plot_perimeters(plot_areas: Sequence[set[complex]]) -> list[int]:
+    perimeters = []
+    for area in plot_areas:
+        perimeters.append(count_continuous_plot_edges(area))
+    return perimeters
+
+
+def calculate_fence_price(
+    garden_plots: dict[complex, str], bounds: tuple[int, int]
+) -> int:
+    adjacent_plots = get_plot_areas(garden_plots, bounds)
+    perimeters = get_full_plot_perimeters(adjacent_plots)
+    total = 0
+    for adjacent_plot, perimeter in zip(adjacent_plots, perimeters):
+        total += len(adjacent_plot) * perimeter
+    return total
+
+
+def calculate_bulk_fence_price(
+    garden_plots: dict[complex, str], bounds: tuple[int, int]
+) -> int:
+    adjacent_plots = get_plot_areas(garden_plots, bounds)
+    perimeters = get_continuous_plot_perimeters(adjacent_plots)
+    total = 0
+    for adjacent_plot, perimeter in zip(adjacent_plots, perimeters):
+        total += len(adjacent_plot) * perimeter
+    return total
+
+
+def part_a_examples():
     fp = r"./example/day12-example01.txt"
     data, bounds = parse_data(fp)
-    adjacent_plots = get_plot_areas(data, bounds)
+    fence_price = calculate_fence_price(data, bounds)
+    print(fence_price, "= 140")
+
+    fp = r"./example/day12-example02.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_fence_price(data, bounds)
+    print(fence_price, "= 772")
+
+    fp = r"./example/day12-example03.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_fence_price(data, bounds)
+    print(fence_price, "= 1930")
+
+
+def part_a():
+    fp = r"./data/day12.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_fence_price(data, bounds)
+    print(fence_price)
+
+
+def part_b_examples():
+    fp = r"./example/day12-example01.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_bulk_fence_price(data, bounds)
+    print(fence_price, "= 80")
+
+    fp = r"./example/day12-example02.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_bulk_fence_price(data, bounds)
+    print(fence_price, "= 436")
+
+    fp = r"./example/day12-example04.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_bulk_fence_price(data, bounds)
+    print(fence_price, "= 236")
+
+    fp = r"./example/day12-example05.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_bulk_fence_price(data, bounds)
+    print(fence_price, "= 368")
+
+    fp = r"./example/day12-example03.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_bulk_fence_price(data, bounds)
+    print(fence_price, "= 1206")
+
+
+def part_b():
+    fp = r"./data/day12.txt"
+    data, bounds = parse_data(fp)
+    fence_price = calculate_bulk_fence_price(data, bounds)
+    print(fence_price)
+
+
+if __name__ == "__main__":
+    part_a_examples()
+    part_a()
+    part_b_examples()
+    part_b()
