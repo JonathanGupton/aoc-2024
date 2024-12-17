1487f1217761e3421e47005e448c1d8f35c2348c
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2024-12-17 04:31:14 -0600

    feat: add day 15 part 1

--- aoc/day15.py
+++ aoc/day15.py
@@ -5,11 +5,24 @@ DIRECTION = {
     "<": complex(-1, 0),
     ">": complex(1, 0),
     "^": complex(0, -1),
-    "V": complex(0, 1),
+    "v": complex(0, 1),
 }
 
 
-def parse_data(fp: str) -> tuple[dict[complex, str], str]:
+def draw_warehouse(
+    warehouse_map: dict[complex, str], dimensions: tuple[int, int]
+) -> str:
+    warehouse_array = [
+        ["." for _ in range(dimensions[0])] for _ in range(dimensions[1])
+    ]
+    for k, v in warehouse_map.items():
+        x, y = int(k.real), int(k.imag)
+        warehouse_array[y][x] = v
+    warehouse_str = "\n".join("".join(row) for row in warehouse_array) + "\n"
+    return warehouse_str
+
+
+def parse_data(fp: str) -> tuple[dict[complex, str], str, tuple[int, int]]:
     warehouse_map = {}
     with open(fp, "r") as f:
         warehouse_map_str, directions = f.read().split("\n\n")
@@ -19,7 +32,8 @@ def parse_data(fp: str) -> tuple[dict[complex, str], str]:
                     warehouse_map[complex(x, y)] = col
 
         directions = "".join(d for d in directions.strip().splitlines())
-    return warehouse_map, directions
+    dimensions = (x + 1, y + 1)
+    return warehouse_map, directions, dimensions
 
 
 def get_robot_position(warehouse_map: dict[complex, str]) -> complex:
@@ -28,15 +42,16 @@ def get_robot_position(warehouse_map: dict[complex, str]) -> complex:
             return k
 
 
-def get_gps_coordinate(object_location: complex) -> int:
-    return int(object_location.real * 100 + object_location.imag)
+def get_single_gps_coordinate_score(object_location: complex) -> int:
+    return int(object_location.real + object_location.imag * 100)
 
 
 def compute_total_gps_score(warehouse_map: dict[complex, str]) -> int:
     total = 0
-    for k, v in warehouse_map:
+    for coordinate, v in warehouse_map.items():
         if v == "O":
-            total += get_gps_coordinate(k)
+            total += get_single_gps_coordinate_score(coordinate)
+    return total
 
 
 def move_is_unobstructed(
@@ -55,28 +70,58 @@ def move_is_unobstructed(
 
 
 def move_robot(warehouse_map: dict[complex, str], direction: str) -> dict[complex, str]:
-    robot_location = get_robot_position(warehouse_map)
-    current_location = robot_location
-    stack = deque()
-    if move_is_unobstructed(current_location, direction, warehouse_map):
-        while True:
-            stack.append(current_location)
-            next_location = current_location + DIRECTION[direction]
-            next_location_object = warehouse_map.get(next_location)
-            if next_location_object is None:
-                while stack:
-                    end = stack.pop()
-                    warehouse_map[end + DIRECTION[direction]] = warehouse_map[end]
-                del warehouse_map[robot_location]
-            else:
-                current_location = next_location
+    direction_vector = DIRECTION[direction]
+    current = get_robot_position(warehouse_map)
+    stack = deque([current])
+    while True:
+        if warehouse_map.get(current + direction_vector) is None:
+            while stack:
+                last = stack.pop()
+                warehouse_map[last + direction_vector] = warehouse_map.get(last)
+            del warehouse_map[last]
+            break
+        if warehouse_map.get(current + direction_vector) == "#":
+            break
+        if warehouse_map.get(current + direction_vector) == "O":
+            current = current + direction_vector
+            stack.append(current)
     return warehouse_map
 
 
-if __name__ == "__main__":
+def example_a():
     fp = r"./example/day15-example01.txt"
-    warehouse_map, directions = parse_data(fp)
-    for direction in directions:
+    warehouse_map, directions, dimensions = parse_data(fp)
+    for i, direction in enumerate(directions):
+        # print(draw_warehouse(warehouse_map, dimensions))
         warehouse_map = move_robot(warehouse_map, direction)
+
+    # print(draw_warehouse(warehouse_map, dimensions))
+    total = compute_total_gps_score(warehouse_map)
+    print(total, "= 2028")
+
+    fp = r"./example/day15-example02.txt"
+    warehouse_map, directions, dimensions = parse_data(fp)
+    for i, direction in enumerate(directions):
+        # print(draw_warehouse(warehouse_map, dimensions))
+        warehouse_map = move_robot(warehouse_map, direction)
+
+    # print(draw_warehouse(warehouse_map, dimensions))
+    total = compute_total_gps_score(warehouse_map)
+    print(total, "= 10092")
+
+
+def part_a():
+    fp = r"./data/day15.txt"
+    warehouse_map, directions, dimensions = parse_data(fp)
+    for i, direction in enumerate(directions):
+        # print(draw_warehouse(warehouse_map, dimensions))
+        warehouse_map = move_robot(warehouse_map, direction)
+
+    # print(draw_warehouse(warehouse_map, dimensions))
     total = compute_total_gps_score(warehouse_map)
     print(total)
+
+
+if __name__ == "__main__":
+    example_a()
+    part_a()
